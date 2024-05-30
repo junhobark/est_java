5ddd4da4205a9ae27688905320d652ab364bd38a
junhobark <wnsh66@naver.com>
2024-05-30 17:24:00 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    /**
+     * @author 굿
+     * @deprecated 사용하지 않는 기능입니다
+     * @since 2021-07-01
+     **/
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
